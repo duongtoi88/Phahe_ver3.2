658af07d9f845446b995f1ed070ef1c58b642d10
duongtoi88 <toidt.ctv@gmail.com>
2026-02-06 13:55:01 +0700

    Update mother-nodes.js

--- mother-nodes.js
+++ mother-nodes.js
@@ -10,16 +10,46 @@
 function renderMotherNodes(g, nodes, peopleMap) {
   const motherData = [];
 
-  nodes.forEach(d => {
-    const motherID = d.data.mother;
-    if (motherID && peopleMap[motherID]) {
+function renderMotherNodes(g, nodes, peopleMap) {
+  const motherData = [];
+
+    nodes.forEach(d => {
+      const motherID = d.data.mother;
+      if (!motherID) return;
+  
+      const mother = peopleMap[motherID];
+      if (!mother) return;
+  
       motherData.push({
-        mother: peopleMap[motherID],
-        x: d.x,     // lệch ngang so với cha
-        y: d.y+20
+        mother,
+        x: d.x + 120,   // lệch phải so với cha
+        y: d.y
       });
-    }
-  });
+    });
+  
+    const mg = g.selectAll(".mother-node")
+      .data(motherData, d => d.mother.id)
+      .enter()
+      .append("g")
+      .attr("class", "mother-node")
+      .attr("transform", d => `translate(${d.x},${d.y})`)
+      .on("click", (e, d) => openDetailTab(d.mother.id));
+  
+    mg.append("rect")
+      .attr("x", -40)
+      .attr("y", -60)
+      .attr("width", 80)
+      .attr("height", 120)
+      .attr("rx", 10)
+      .attr("ry", 10)
+      .attr("class", "mother-rect");
+  
+    mg.append("text")
+      .attr("text-anchor", "middle")
+      .attr("fill", "black")
+      .text(d => d.mother.name);
+  }
+
 
   const motherGroup = g.selectAll(".mother-node")
     .data(motherData)
@@ -43,4 +73,5 @@ function renderMotherNodes(g, nodes, peopleMap) {
     .style("font-size", "11px")
     .text(d => d.mother.name);
 }
+
 
