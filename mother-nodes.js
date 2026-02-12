75a39e8e2ce41e6fb051748221ea3bc4eb59da56
duongtoi88 <toidt.ctv@gmail.com>
2026-02-12 08:43:46 +0700

    Update mother-nodes.js

--- mother-nodes.js
+++ mother-nodes.js
@@ -16,7 +16,7 @@ window.MotherNodes = (function () {
       .attr("x", -40)
       .attr("y", -30)
       .attr("width", 80)
-      .attr("height", 120)
+      .attr("height", 60)
       .attr("rx", 10)
       .attr("ry", 10)
       .attr("fill", "#ffe5e5")
@@ -33,4 +33,5 @@ window.MotherNodes = (function () {
   return { render };
 
 })();
+
 
